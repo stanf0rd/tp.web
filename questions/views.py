a9d90fa53648ebda9bee99cfb9655b1b0fdc6c81
stanford <dmitrydpaliy@gmail.com>
2018-05-14 22:48:24 +0300

    simple views improved

--- questions/views.py
+++ questions/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the questions index.")
+
+def hot(request):
+    return HttpResponse("You're looking at hot questions.")
+
+def tag(request, tag):
+    return HttpResponse("You're looking at tag \"%s\" questions." % tag)
+
+def question(request, question_id):
+    return HttpResponse("You're looking at question number %s." % question_id)
+
+def login(request):
+    return HttpResponse("You're looking at login page.")
+
+def signup(request):
+    return HttpResponse("You're looking at signup page.")
+
+def ask(request):
+    return HttpResponse("You're looking at ask page.")
