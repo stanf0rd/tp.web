848829e33e8a26e4a21ec00314bfb2400e443959
stanford <dmitrydpaliy@gmail.com>
2018-05-22 12:01:46 +0300

    models registered

--- questions/admin.py
+++ questions/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from questions.models import Question, User, Tag
 
 # Register your models here.
+
+admin.site.register(Question)
+admin.site.register(User)
+admin.site.register(Tag)
